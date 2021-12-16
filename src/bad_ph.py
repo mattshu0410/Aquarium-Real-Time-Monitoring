7278d2557df90605651db9b6c4e3660cd229c3f1
Matthew Shu <matthews1000140@gmail.com>
2021-12-16 20:16:21 +1100

    Final Commit

---
+++ src/bad_ph.py
@@ -0,0 +1,10 @@
+import simplejson as json # instead of import json
+import uuid
+import boto3
+import time
+from datetime import datetime
+from datetime import timezone
+import random
+from decimal import Decimal
+
+
--- src/kinesis_stream.py
+++ src/kinesis_stream.py
@@ -15,49 +15,81 @@ from decimal import Decimal
     #5. python kinesis_stream.py to run python script
 
 
+def get_nh3():
+    return round(random.uniform(0, 1), 1)
 
+    # return round(random.random())
 
-#print(str(timestamp))
-#print(json.dumps(data))
+def get_no3():
+    return round(random.uniform(0, 1), 1)
+
+    # return round(random.random())
+
+def get_no2():
+    if round(random.random()) == 1:
+        return random.randrange(40, 100)
+    else:
+        return random.randrange(1, 40)
+
+def get_ph():
+	n = random.choice([1]*10 + [2]*80 + [3]*10)
+	#if n == 1:
+	#	return round(random.uniform(1, 6.5), 1)
+	#elif n==2:
+	#	return round(random.uniform(7.5, 14), 1)
+	#else:
+	#	return round(random.uniform(6.5, 7.5), 1)
+	return round(random.uniform(1, 6.5), 1)
+
+client = boto3.client('kinesis')
+while True:
+	for metric in ['no3', 'no2', 'ph', 'nh3']:
+		time_stamp =  datetime.strftime((datetime.now(timezone.utc)), '%Y-%m-%dT%H:%M:%SZ')
+		record = {
+			'metric': metric,
+			'time_stamp': str(time_stamp),
+			'metric_value': eval("get_"+metric+"()")
+			}
+		response = client.put_record(
+			StreamName = "water-metric-streaming",
+			Data = json.dumps(record),
+			PartitionKey = str(record['metric'])
+		)	
+		print(f'Record added for {record["metric"]}')
+		time.sleep(1)
 
 # Create a kinesis client
-client = boto3.client('kinesis')
-
-# Send message to Kinesis DataStream
-for i in range(1,100):
-	time_stamp =  datetime.strftime((datetime.now(timezone.utc)), '%Y-%m-%dT%H:%M:%SZ')
-	print(time_stamp)
-	data = {
-		'metric': random.choice(['no3', 'no2', 'ph', 'nh3']),
-    	'time_stamp': str(time_stamp),
-    	'metric_value': random.random()
-	}
-	response = client.put_record(
-		StreamName = "water-metric-streaming",
-		Data = json.dumps(data),
-		PartitionKey = str(data['metric'])
-	)
-	# If the message was not successfully sent print an error message
-	if response['ResponseMetadata']['HTTPStatusCode'] != 200:
-		print('Error!')
-	else:
-		print('Message sent')
-		print(data)
-	time.sleep(0.5)
-
-
-#data = {
-#'timestamp': str(timestamp),
-#'metric': 'no2',
-#'value': str(random.random())
-#}
+#client = boto3.client('kinesis')
+#def create_data():
+#	Records = []
+#	time_stamp =  datetime.strftime((datetime.now(timezone.utc)), '%Y-%m-%dT%H:%M:%SZ')
+#	print(time_stamp)
+#	for metric in ['no3', 'no2', 'ph', 'nh3']:
+#		record = {
+#			'metric': metric,
+#    		'time_stamp': str(time_stamp),
+#    		'metric_value': eval("get_"+metric+"()")
+#		}
+#		Records.append(record)
+#	return Records
 #
-#response = client.put_record(
-#		StreamName = "water-metric-streaming",
-#		Data = json.dumps(data),
-#		PartitionKey = str(data['timestamp'])
-#)
-#print(response)
-
+## Send message to Kinesis DataStream
+#while True:
+#	Records = create_data()
+#	for record in Records:
+#		response = client.put_record(
+#			StreamName = "water-metric-streaming",
+#			Data = json.dumps(record),
+#			PartitionKey = str(record['metric'])
+#		)
+#		print(f'Record added for {record["metric"]}')
+#		time.sleep(1)
+#	# If the message was not successfully sent print an error message
+#	if response['ResponseMetadata']['HTTPStatusCode'] != 200:
+#		print('Error!')
+#	else:
+#		print('Message sent')
+#		print(Records)
+#	time.sleep(2)
 
 
